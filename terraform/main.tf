e254c1d604d7bb6874852bfeb8d92d68de9f4ddd
root <root@DESKTOP-6JPT7JG.>
2025-06-08 14:56:36 -0400

    Added alias in r53

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,6 +65,31 @@ resource "aws_route53_record" "cert_validation" {
   ttl     = 60
 }
 
+resource "aws_route53_record" "alias" {
+  zone_id = aws_route53_zone.zone.zone_id
+  name    = "whatjacketdoineed.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.zone.zone_id
+  name    = "www.whatjacketdoineed.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
